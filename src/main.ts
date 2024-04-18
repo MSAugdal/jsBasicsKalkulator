3ee2620b0f4bc880e33c1dec3751b5f8ad8ed5ee
MSAugdal <mathias.s.augdal@gmail.com>
2024-04-18 13:12:43 +0200

    💄 UI update for calculator
    added styling for calculator and added a display for it

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div id="wrapper" class="bg-grey-900 p-3 rounded grid grid-cols-[1fr_1fr_1fr_1fr] grid-rows-[1fr_1fr_1fr_1fr_1fr] gap-2 justify-center items-center ml-auto mr-auto w-3/12 min-h-fit">
+  <div id="display"></div>
+  <div id="wrapper" class="bg-grey-900 p-3 rounded grid grid-cols-4 grid-rows-6 gap-2 justify-center items-center ml-auto mr-auto w-3/12 min-h-fit">
   </div>
 `
 const calc_wrapper: HTMLDivElement | null = document.querySelector<HTMLDivElement>('#wrapper')!
+
+const calc_display: HTMLDivElement = document.createElement("div");
+const disp_style: Array<string> = ["col-span-4", "bg-white", "rounded", "p-3", "pt-9", "pb-9", "text-xl"];
+disp_style.forEach((style) => {
+  calc_display.classList.add(style);
+});
+calc_wrapper.appendChild(calc_display);
+
 const buttons: Array<string> = [
   'AC', '+/-', '%', '/',
   '7', '8', '9', '*',
@@ -12,8 +21,8 @@ const buttons: Array<string> = [
   '0', ',', '='
 ];
 
+const styling: Array<string> = ["hover:scale-[1.02]", "hover:bg-orange-300", "w-[100%]", "h-[100%]", "rounded", "flex", "justify-center", "items-center", "bg-orange-400", "p-3", "m-auto"]
 buttons.forEach((value: string) => {
-  const styling: Array<string> = ["w-[100%]", "h-[100%]", "rounded", "flex", "justify-center", "items-center", "bg-orange-400", "p-3", "m-auto"]
   let buttonElement: HTMLDivElement = document.createElement("div");
 
   if (value === '0') buttonElement.classList.add("col-span-2");
