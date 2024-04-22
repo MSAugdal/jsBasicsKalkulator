8600346ef70cdb80358ee557bd7198763e26cca2
MSAugdal <mathias.s.augdal@gmail.com>
2024-04-22 11:20:59 +0200

    ♻️ Refactored code
    refactored code

--- src/main.js
+++ src/main.js
@@ -1,50 +1,51 @@
 document.querySelector('#app').innerHTML = "\n  <div id=\"wrapper\" class=\"bg-grey-900 p-3 rounded grid grid-cols-4 grid-rows-6 gap-2 justify-center items-center mx-auto my-auto max-h-96\">\n    <div class=\"text-black col-span-4 bg-white rounded px-3 py-4\"><p id=\"display\" class=\"h-5\"></p></div>\n  </div>\n";
 var calc_wrapper = document.querySelector('#wrapper');
-var display = document.querySelector('#display');
-var evalString = [];
-var symbols = ['+', '-', '*', '/', '%'];
 var buttons = [
     'AC', 'DEL', '%', '/',
-    '7', '8', '9', '*',
-    '4', '5', '6', '-',
-    '1', '2', '3', '+',
-    '0', ',', '='
+    7, 8, 9, '*',
+    4, 5, 6, '-',
+    1, 2, 3, '+',
+    0, '.', '='
 ];
-var handleCLick = function (e) {
+var evalArr = '';
+var display = document.querySelector('#display');
+var Symbols;
+(function (Symbols) {
+    Symbols["DEL"] = "DEL";
+    Symbols["AC"] = "AC";
+})(Symbols || (Symbols = {}));
+function handleCLick(e) {
     var target = e.target;
     if (target === null)
-        return "Error: target of event \"".concat(e, "\" not found");
-    if (symbols.indexOf(target.id) !== -1 && evalString.length === 0)
+        return console.log("Error: target of event \"".concat(e, "\" not found"));
+    if (target.id == '=') {
+        evalArr = eval(evalArr).toString();
+        display.innerHTML = evalArr;
         return;
-    if (symbols.indexOf(target.id) !== -1 && symbols.indexOf(evalString[evalString.length - 1]) !== -1)
-        return;
-    if (target.id === '=') {
-        if (evalString.length === 0)
-            return;
-        if (symbols.indexOf(evalString[evalString.length - 1]) !== -1)
-            return;
-        evalString = [eval(evalString.join(''))];
-        display.innerHTML = evalString.join('');
     }
-    else if (target.id === 'DEL') {
-        if (evalString.length === 0)
-            return;
-        evalString.pop();
-        display.innerHTML = evalString.join('');
+    else if (target.id == Symbols.AC) {
+        evalArr = '';
     }
-    else if (target.id === 'AC') {
-        evalString = [];
-        display.innerHTML = evalString.join('');
+    else if (target.id == Symbols.DEL) {
+        evalArr = evalArr.slice(0, evalArr.length - 1);
+    }
+    else if (isNaN(+target.id)) {
+        if (!isNaN(+evalArr[evalArr.length - 1])) {
+            console.log("sym");
+            evalArr = evalArr.concat(target.id);
+        }
     }
     else {
-        evalString.push(target.id);
-        display.innerHTML = evalString.join('');
+        console.log("num");
+        evalArr = evalArr.concat(target.id);
     }
-};
+    display.innerHTML = evalArr;
+    return;
+}
 var styling = ["hover:scale-[1.02]", "hover:bg-orange-300", "w-[100%]", "h-[100%]", "rounded", "flex", "justify-center", "items-center", "bg-orange-400", "p-3", "m-auto"];
 buttons.forEach(function (value) {
     var buttonElement = document.createElement("div");
-    if (value === '0')
+    if (value == '0')
         buttonElement.classList.add("col-span-2");
     styling.forEach(function (style) {
         buttonElement.classList.add(style);
@@ -53,6 +54,6 @@ buttons.forEach(function (value) {
         handleCLick(e);
     });
     buttonElement.id = value;
-    buttonElement.innerHTML = "<p class=\"pointer-events-none\">".concat(value, "</p>");
+    buttonElement.innerHTML = "".concat(value);
     calc_wrapper.appendChild(buttonElement);
 });
--- src/main.ts
+++ src/main.ts
@@ -1,55 +1,62 @@
-
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div id="wrapper" class="bg-grey-900 p-3 rounded grid grid-cols-4 grid-rows-6 gap-2 justify-center items-center mx-auto my-auto max-h-96">
     <div class="text-black col-span-4 bg-white rounded px-3 py-4"><p id="display" class="h-5"></p></div>
   </div>
 `
 const calc_wrapper: HTMLDivElement | null = document.querySelector<HTMLDivElement>('#wrapper')!
-const display: HTMLDivElement | null = document.querySelector<HTMLDivElement>('#display');
 
-let evalString: Array<string> = [];
-const symbols: Array<string> = ['+', '-', '*', '/', '%'];
-const buttons: Array<string> = [
+const buttons: Array<string | number> = [
   'AC', 'DEL', '%', '/',
-  '7', '8', '9', '*',
-  '4', '5', '6', '-',
-  '1', '2', '3', '+',
-  '0', ',', '='
+  7, 8, 9, '*',
+  4, 5, 6, '-',
+  1, 2, 3, '+',
+  0, '.', '='
 ];
 
-const handleCLick = (e: Event) => {
+let evalArr: string = '';
+const display: HTMLInputElement | null = document.querySelector<HTMLInputElement>('#display');
+
+enum Symbols {
+  DEL = "DEL",
+  AC = "AC",
+}
+
+
+function handleCLick(e: Event): void {
   const target: HTMLDivElement | null = <HTMLDivElement>e.target;
-  if (target === null) return `Error: target of event "${e}" not found`;
-
-  if (symbols.indexOf(target.id) !== -1 && evalString.length === 0) return;
-  if (symbols.indexOf(target.id) !== -1 && symbols.indexOf(evalString[evalString.length - 1]) !== -1) return;
-
-  if (target.id === '=') {
-    if (evalString.length === 0) return;
-    if (symbols.indexOf(evalString[evalString.length - 1]) !== -1) return;
-    evalString = [eval(evalString.join(''))];
-    display!.innerHTML = evalString.join('');
+  if (target === null) return console.log(`Error: target of event "${e}" not found`);
+  if (target.id == '=') {
+    evalArr = eval(evalArr).toString();
+    display!.innerHTML = evalArr;
+    return
   }
-  else if (target.id === 'DEL') {
-    if (evalString.length === 0) return;
-    evalString.pop();
-    display!.innerHTML = evalString.join('');
+
+  else if (target.id == Symbols.AC) {
+    evalArr = '';
   }
-  else if (target.id === 'AC') {
-    evalString = [];
-    display!.innerHTML = evalString.join('');
+  else if (target.id == Symbols.DEL) {
+    evalArr = evalArr.slice(0, evalArr.length - 1);
+  }
+  else if (isNaN(+target.id)) {
+    if (!isNaN(+evalArr[evalArr.length - 1])) {
+      console.log("sym");
+      evalArr = evalArr.concat(target.id);
+    }
   }
   else {
-    evalString.push(target.id);
-    display!.innerHTML = evalString.join('');
+    console.log("num");
+    evalArr = evalArr.concat(target.id);
   }
-};
+  display!.innerHTML = evalArr;
+  return
+}
+
 
 const styling: Array<string> = ["hover:scale-[1.02]", "hover:bg-orange-300", "w-[100%]", "h-[100%]", "rounded", "flex", "justify-center", "items-center", "bg-orange-400", "p-3", "m-auto"]
-buttons.forEach((value: string) => {
+buttons.forEach((value: string | number) => {
   let buttonElement: HTMLDivElement = document.createElement("div");
 
-  if (value === '0') buttonElement.classList.add("col-span-2");
+  if (value == '0') buttonElement.classList.add("col-span-2");
 
   styling.forEach((style) => {
     buttonElement.classList.add(style);
@@ -59,8 +66,8 @@ buttons.forEach((value: string) => {
     handleCLick(e);
   });
 
-  buttonElement.id = value;
-  buttonElement.innerHTML = `<p class="pointer-events-none">${value}</p>`;
+  buttonElement.id = <string>value;
+  buttonElement.innerHTML = `${value}`;
   calc_wrapper.appendChild(buttonElement);
 });
 
